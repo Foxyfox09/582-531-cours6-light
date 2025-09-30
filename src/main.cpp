4427c262b88412fb60dba2bc686ee1cd25e6b847
Foxyfox09 <2396998@cmontmorency.qc.ca>
2025-09-30 17:48:59 -0400

    Fin de cours

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,44 @@
-#include <Arduino.h>
+// Le code minimal
 
-// put function declarations here:
-int myFunction(int, int);
+#include <Arduino.h> 
+#include <MicroOscSlip.h>
+#include <FastLED.h>
+CRGB monPixel;
+
+#define MA_BROCHE_ANGLE 32
+
+MicroOscSlip<128> monOsc(&Serial);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+Serial.begin(115200);  
+FastLED.addLeds<WS2812,27,GRB>( & monPixel, 1  );
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  int maLectureAnalogique;
+   maLectureAnalogique = analogRead(MA_BROCHE_ANGLE );
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+   monOsc.sendInt( "/angle" , maLectureAnalogique);
+
+   Serial.print(maLectureAnalogique);
+   Serial.println();
+  delay(100);
+
+
+    if (maLectureAnalogique < 1365) {
+    monPixel = CRGB(0,0,255);
+  } else if (maLectureAnalogique >= 1365 && maLectureAnalogique <= 2730) {
+    monPixel = CRGB(0,255,0);
+  } else if (maLectureAnalogique > 2730) {
+    monPixel = CRGB(255,0,0);
+  }
+    FastLED.show();
+
+  // if (maLectureAnalogique > 2048) {
+  //   monPixel = CRGB(255,0,0);
+  //   FastLED.show();
+  // } else if (maLectureAnalogique < 2048) {
+  //   monPixel = CRGB(0,0,0);
+  //   FastLED.show();
+  // }
 }
